f95c92c928167e6ff73efa362150037d511d77e0
lukblan <luk.blanko@gmail.com>
2024-07-28 19:58:35 -0300

    Add cat table and model

---
+++ app/models/cat.rb
@@ -0,0 +1,22 @@
+class Cat < ApplicationRecord
+  COLORS = ["cream", "white", "orange", "brown", "blue-gray", "black"]
+  VALID_SEX = ["M", "F"]
+
+  validates :name, :birth_date, :description, :color, :sex, presence: true
+  validates :color, inclusion: COLORS
+  validates :sex, inclusion: VALID_SEX
+
+  def age
+    birthday = self.birth_date
+    today_date = Date.today
+    total_years = today_date.year - birthday.year
+
+    if birthday.month > today_date.month
+      total_years -= 1
+    elsif birthday.month == today_date.month && birthday.day > today_date.day
+      total_years -= 1
+    end
+
+    p total_years
+  end
+end
