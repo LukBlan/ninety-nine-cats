163b8c67d0559e11b5953c2723d4bec445043fd0
lukblan <luk.blanko@gmail.com>
2024-07-29 18:25:14 -0300

    Add cat index and show routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,19 @@
 miguel = Cat.create!(
   birth_date: "2023/7/28",
+  name: "Miguel",
+  color: "orange",
+  description: "I found white in the streets",
+  sex: "M"
+)
+
+perla = Cat.create!(
+  birth_date: "2015/4/15",
   name: "Perla",
   color: "black",
   description: "I found perla in the streets",
   sex: "F"
 )
 
+perla.save
 miguel.save
 
