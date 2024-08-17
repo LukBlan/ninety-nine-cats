515e2c1ce1e9993e547c14d79c94aa759feadc78
lukblan <luk.blanko@gmail.com>
2024-08-17 19:34:20 -0300

    Add new resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "roots#index"
 
+  resource :session
+  resources :users
   resources :cats, only: [:index, :show, :new, :create, :edit, :update]
   resources :cat_rental_requests, only: [:new, :create] do
     member do
